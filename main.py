c841c95ee55db285d5af9eee7ef5db4f3d5bfc2a
PELENIUM <i.d.chernyshov@yandex.ru>
2024-03-03 21:02:34 +0300

    added bordering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 import cv2
+import math
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1080)
@@ -8,41 +9,39 @@ cap.set(4, 720)
 model = YOLO("runs/detect/result/weights/best.pt")
 
 while True:
+    defectCounter = 0
     success, img = cap.read()
-    img = cv2.resize(img, (640, 640))
+    img = cv2.flip(cv2.resize(img, (640, 640)), 1)
     results = model(img, stream=True, verbose=False)
-    # results = model.predict("test.jpg")
 
     for r in results:
         boxes = r.boxes
 
         for box in boxes:
             cls = int(box.cls[0])
-            cls = "defect"
-            print(cls)
-            # 0 = human
-    #         if cls == 0:
-    #             peopleCounter += 1
+            
+            if cls == 0:
+                defectCounter += 1
 
-    #             x1, y1, x2, y2 = box.xyxy[0]
-    #             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
-    #             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-    #             confidence = math.ceil((box.conf[0]*100))/100
-    #             print("Person found, Confidence --->", confidence)
+                confidence = math.ceil((box.conf[0]*100))/100
+                print("Defect found, Confidence --->", confidence)
 
-    #             org = [x1, y1]
-    #             font = cv2.FONT_HERSHEY_SIMPLEX
-    #             fontScale = 1
-    #             color = (255, 0, 0)
-    #             thickness = 2
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
 
-    #             cv2.putText(img, "person", org,
-    #                         font, fontScale, color, thickness)
+                cv2.putText(img, "defect", org,
+                            font, fontScale, color, thickness)
 
-    # cv2.putText(img, f"People in picture: {peopleCounter}",
-    #             [10, 20], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+    cv2.putText(img, f"Defects in picture: {defectCounter}",
+                [10, 20], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
@@ -50,19 +49,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-# from ultralytics import YOLO
-
-
-
-# result = results[0]
-
-# print(len(result.boxes))
-
-# box = result.boxes[0]
-
-# print("Object type:", box.cls)
-# print("Coordinates:", box.xyxy)
-# print("Probability:", box.conf)
